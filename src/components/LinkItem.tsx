2dc6cfcf44c05fdb8ec1c4f88a364c08baafbd59
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-08-13 11:54:41 -0800

    Save current component

---
+++ src/components/LinkItem.tsx
@@ -0,0 +1,22 @@
+import type { Link } from "../types/Link";
+
+interface Props {
+    link: Link;
+    onEdit: (link: Link) => void;
+    onDelete: (id: number) => void;
+}
+
+export default function LinkItem({ link, onEdit, onDelete }: Props) {
+    return (
+        <div className="link-item">
+        <h3>{link.title}</h3>
+        <a href={link.url} target="_blank">{link.url}</a>
+        <p>{link.description}</p>
+        {link.tags && <small>Tags: {link.tags}</small>}
+        <div className = "buttons">
+            <button onClick={() => onEdit(link)}>Edit</button>
+            <button onClick={() => onDelete(link.id)}></button>
+        </div>
+        </div>
+    );
+}
