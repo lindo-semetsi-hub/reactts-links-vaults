64a6669d46c713f982b10a39c4e5279df3c61cd0
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-08-14 02:54:17 +0200

    Save all changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import type { Link } from "./types/Link";
+import LinkForm from "./components/LinkForm";
+import LinkList from "./components/Linklist";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [links, setLinks] = useState<Link[]>([]);
+  const [editingLink, setEditingLink] = useState<Link | undefined>(undefined);
+  const [search, setSearch] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  useEffect(() => {
+    const saved = localStorage.getItem("links");
+    if (saved) setLinks(JSON.parse(saved));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("links", JSON.stringify(links));
+  }, [links]);
+
+  const handleSave = (link: Link) => {
+    if (editingLink) {
+      setLinks(links.map(l => (l.id === link.id ? link : l)));
+      setEditingLink(undefined);
+    } else {
+      setLinks([...links, link]);
+    }
+  };
+
+  const handleEdit = (link: Link) => {
+    setEditingLink(link);
+  };
+
+  const handleDelete = (id: number) => {
+    setLinks(links.filter(l => l.id !== id));
+  };
+
+  const filteredLinks = links.filter(l =>
+    l.title.toLowerCase().includes(search.toLowerCase()) ||
+    l.url.toLowerCase().includes(search.toLowerCase()) ||
+    l.description.toLowerCase().includes(search.toLowerCase()) ||
+    (l.tags && l.tags.toLowerCase().includes(search.toLowerCase()))
+    );
+
+    return (
+      <div className="container">
+        <h1>Links Vault</h1>
+
+        {/* Search box */}
+        <input
+        type="text"
+        placeholder="Search links..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        />
+        {/* Link form */}
+        <LinkForm onSave={handleSave} editingLink={editingLink} />
+
+        {/* Link form */}
+        <LinkList links={filteredLinks} onEdit={handleEdit} onDelete={handleDelete} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
 }
-
-export default App
